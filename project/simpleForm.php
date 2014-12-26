24d077e4ad8aea69d8d2fb9c63fd620463b7bfd7
Weiqi Lu <luxweiqi@gmail.com>
2014-12-26 16:02:12 +0800

    sample page

---
+++ project/simpleForm.php
@@ -0,0 +1,33 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+if (isset($_POST['action']) && $_POST['action'] == 'submitted') {
+    echo '<pre>';
+
+    print_r($_POST);
+    echo '<a href="'. $_SERVER['PHP_SELF'] .'">Please try again</a>';
+
+    echo '</pre>';
+} else {
+?>
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+    Name:  <input type="text" name="personal[name]"><br />
+    Email: <input type="text" name="personal[email]"><br />
+    Beer: <br>
+    <select multiple name="beer[]">
+        <option value="warthog">Warthog</option>
+        <option value="guinness">Guinness</option>
+        <option value="stuttgarter">Stuttgarter Schwabenbr</option>
+    </select><br />
+    <input type="hidden" name="action" value="submitted" />
+    <input type="submit" name="submit" value="submit me!" />
+</form>
+<?php
+}
+?>
+
+
